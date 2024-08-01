cd303ffc77dfc9260bc5a114b2d166e6fc628200
Yu <Dynamite1864@163.com>
2024-08-01 11:46:56 +0800

    Initial commit

---
+++ src/main/java/org/learnKafka/test/consumer/KafkaConsumerTest.java
@@ -0,0 +1,39 @@
+package org.learnKafka.test.consumer;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+
+import java.time.Duration;
+import java.util.Collections;
+import java.util.HashMap;
+
+public class KafkaConsumerTest {
+    public static void main(String[] args) {
+
+        HashMap<String, Object> consumerConfig = new HashMap<>();
+        consumerConfig.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        consumerConfig.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        consumerConfig.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        consumerConfig.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+
+        consumerConfig.put(ConsumerConfig.GROUP_ID_CONFIG, "test");
+
+        //Create Consumer
+        KafkaConsumer<String, String> kafkaConsumer = new KafkaConsumer<>(consumerConfig);
+
+        //subscribe topic
+        kafkaConsumer.subscribe(Collections.singletonList("test"));
+        //get data from Kafka topic
+        while (true) {
+            ConsumerRecords<String, String> records = kafkaConsumer.poll(Duration.ofMillis(100));
+
+            for (ConsumerRecord<String, String> record : records) {
+                System.out.println(record);
+            }
+        }
+
+    }
+}
